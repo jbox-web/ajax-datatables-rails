2b1715598ab52e4d61189ecac227d3cfe66034ee
Antonio Antillon <antillas21@gmail.com>
2014-08-31 09:49:08 -0700

    Edits typecast. We need a VARCHAR instead of a single CHAR.

--- lib/ajax-datatables-rails/base.rb
+++ lib/ajax-datatables-rails/base.rb
@@ -93,7 +93,7 @@ module AjaxDatatablesRails
     def search_condition(column, value)
       model, column = column.split('.')
       model = model.singularize.titleize.gsub( / /, '' ).constantize
-      casted_column = Arel::Nodes::NamedFunction.new('CAST', [model.arel_table[column.to_sym].as('CHAR')])
+      casted_column = ::Arel::Nodes::NamedFunction.new('CAST', [model.arel_table[column.to_sym].as('VARCHAR')])
       casted_column.matches("%#{value}%")
     end
 
