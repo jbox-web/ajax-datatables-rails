6cf0fd56d95d16ebf0ec2cced59478a82110ee0b
Pierre Yager <pierre@levosgien.net>
2015-12-22 00:35:50 +0100

    Cleanup: removed commented lines

--- lib/ajax-datatables-rails/orm/active_record.rb
+++ lib/ajax-datatables-rails/orm/active_record.rb
@@ -19,12 +19,6 @@ module AjaxDatatablesRails
           column = column_def.column
           queries << column_def.query(column.sort_query) if column
         end
-
-        #sort_by = connected_columns.each_with_object([]) do |(column, column_def), queries|
-        #  order = datatable.order(:column_index, column.index)
-        #  queries << order.query(column.sort_query) if order
-        #end
-
         records.order(sort_by.join(", "))
       end
 
