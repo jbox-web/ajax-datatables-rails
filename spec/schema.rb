e92dd6bf6f45a447e90ee8c214319a19ee946e53
Nicolas Rodriguez <nrodriguez@jbox-web.com>
2017-04-11 00:40:27 +0200

    Remove useless models

--- spec/schema.rb
+++ spec/schema.rb
@@ -10,34 +10,4 @@ ActiveRecord::Schema.define do
     t.timestamps null: false
   end
 
-  create_table :addresses, :force => true do |t|
-    t.string :address_line1
-    t.string :address_line2
-    t.string :city
-    t.string :zip_code
-    t.string :state
-    t.string :country
-
-    t.timestamps null: false
-  end
-
-  create_table :purchased_orders, :force => true do |t|
-    t.string :foo
-    t.string :bar
-
-    t.timestamps null: false
-  end
-
-  create_table :statistics_requests, :force => true do |t|
-    t.string :baz
-
-    t.timestamps null: false
-  end
-
-  create_table :statistics_sessions, :force => true do |t|
-    t.string :foo
-    t.integer :bar
-
-    t.timestamps null: false
-  end
 end
