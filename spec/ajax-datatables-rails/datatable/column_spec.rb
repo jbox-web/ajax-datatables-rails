5fb517f43946bfb760ac28f998b769bc753b9e2d
Nicolas Rodriguez <nrodriguez@jbox-web.com>
2017-04-11 00:49:03 +0200

    Cleanup ColumnSpec describe

--- spec/ajax-datatables-rails/datatable/column_spec.rb
+++ spec/ajax-datatables-rails/datatable/column_spec.rb
@@ -1,64 +1,66 @@
 require 'spec_helper'
 
 describe 'AjaxDatatablesRails::Datatable::Column' do
+
   let(:view) { double('view', params: sample_params) }
   let(:datatable) { ComplexDatatable.new(view) }
-  before {
-    datatable.params[:columns] = {"0"=>{"data"=>"username", "name"=>"", "searchable"=>"true", "orderable"=>"true", "search"=>{"value"=>"searchvalue", "regex"=>"false"}}}
-  }
 
-  describe 'helper methods' do
-    describe 'order methods' do
-      let(:column) { datatable.datatable.columns.first }
 
-      it 'should be orderable' do
-        expect(column.orderable?).to eq(true)
+  describe 'username column' do
+
+    let(:column) { datatable.datatable.columns.first }
+
+    before do
+      datatable.params[:columns] = {"0"=>{"data"=>"username", "name"=>"", "searchable"=>"true", "orderable"=>"true", "search"=>{"value"=>"searchvalue", "regex"=>"false"}}}
+    end
+
+    it 'should be orderable' do
+      expect(column.orderable?).to eq(true)
+    end
+
+    it 'should be searchable' do
+      expect(column.searchable?).to eq(true)
+    end
+
+    it 'should have connected to id column' do
+      expect(column.data).to eq('username')
+    end
+
+    context '#search' do
+      it 'child class' do
+        expect(column.search).to be_a(AjaxDatatablesRails::Datatable::SimpleSearch)
       end
 
-      it 'should be searchable' do
-        expect(column.searchable?).to eq(true)
+      it 'should have search value' do
+        expect(column.search.value).to eq('searchvalue')
       end
 
-      it 'should have connected to id column' do
-        expect(column.data).to eq('username')
+      it 'should not regex' do
+        expect(column.search.regexp?).to eq false
       end
+    end
 
-      context '#search' do
-        it 'child class' do
-          expect(column.search).to be_a(AjaxDatatablesRails::Datatable::SimpleSearch)
-        end
-
-        it 'should have search value' do
-          expect(column.search.value).to eq('searchvalue')
-        end
-
-        it 'should not regex' do
-          expect(column.search.regexp?).to eq false
-        end
+    describe '#cond' do
+      it 'should be :like by default' do
+        expect(column.cond).to eq(:like)
       end
+    end
 
-      describe '#cond' do
-        it 'should be :like by default' do
-          expect(column.cond).to eq(:like)
-        end
+    describe '#source' do
+      it 'should be :like by default' do
+        expect(column.source).to eq('User.username')
       end
+    end
 
-      describe '#source' do
-        it 'should be :like by default' do
-          expect(column.source).to eq('User.username')
-        end
+    describe '#search_query' do
+      it 'should buld search query' do
+        expect(column.search_query.to_sql).to include('%searchvalue%')
       end
+    end
 
-      describe '#search_query' do
-        it 'should buld search query' do
-          expect(column.search_query.to_sql).to include('%searchvalue%')
-        end
-      end
-
-      describe '#sort_query' do
-        it 'should build sort query' do
-          expect(column.sort_query).to eq('users.username')
-        end
+    describe '#sort_query' do
+      it 'should build sort query' do
+        expect(column.sort_query).to eq('users.username')
       end
     end
   end
