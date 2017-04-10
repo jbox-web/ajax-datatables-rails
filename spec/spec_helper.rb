b8391f15b229b9d747e568d94504c3d2cacca226
Nicolas Rodriguez <nrodriguez@jbox-web.com>
2017-04-10 23:21:28 +0200

    Add simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,17 @@
+require 'simplecov'
+require 'rspec'
 require 'pry'
 require 'rails'
 require 'active_record'
+
+# Start Simplecov
+SimpleCov.start do
+  add_filter 'spec/'
+end
+
 require 'ajax-datatables-rails'
 
-ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
+ActiveRecord::Base.establish_connection adapter: 'sqlite3', database: ':memory:'
 
 load File.dirname(__FILE__) + '/schema.rb'
 load File.dirname(__FILE__) + '/test_helpers.rb'
