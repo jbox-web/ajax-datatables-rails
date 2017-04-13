4005fe301d16286ce374691a9038be511feed17d
Nicolas Rodriguez <nrodriguez@jbox-web.com>
2017-04-14 00:51:13 +0200

    Minor change

--- spec/ajax-datatables-rails/extended_spec.rb
+++ spec/ajax-datatables-rails/extended_spec.rb
@@ -25,7 +25,7 @@ describe AjaxDatatablesRails::Base do
       create(:user, username: 'msmith', email: 'mary.smith@example.com', last_name: 'Smith', created_at: '01/02/2000')
     end
 
-    context 'when no range is passed' do
+    context 'when range is empty' do
       it 'should not filter records' do
         datatable.params[:columns]['4'][:search][:value] = '-'
         expect(datatable.data.size).to eq 2
@@ -34,21 +34,21 @@ describe AjaxDatatablesRails::Base do
       end
     end
 
-    context 'when start_date is passed' do
+    context 'when start date is filled' do
       it 'should filter records created after this date' do
         datatable.params[:columns]['4'][:search][:value] = '31/12/1999-'
         expect(datatable.data.size).to eq 2
       end
     end
 
-    context 'when end_date is passed' do
+    context 'when end date is filled' do
       it 'should filter records created before this date' do
         datatable.params[:columns]['4'][:search][:value] = '-31/12/1999'
         expect(datatable.data.size).to eq 0
       end
     end
 
-    context 'when both date are passed' do
+    context 'when both date are filled' do
       it 'should filter records created between the range' do
         datatable.params[:columns]['4'][:search][:value] = '01/12/1999-15/01/2000'
         expect(datatable.data.size).to eq 1
