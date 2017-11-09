1526ae4b42db906ce2a334409d3bc69026f0d5ff
Nicolas Rodriguez <nicoladmin@free.fr>
2017-11-09 16:54:34 +0100

    Update travis-oracle version

--- spec/install_oracle.sh
+++ spec/install_oracle.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget 'https://github.com/cbandy/travis-oracle/archive/v2.0.2.tar.gz'
+wget 'https://github.com/cbandy/travis-oracle/archive/v2.0.3.tar.gz'
 mkdir -p ~/.travis/oracle
-tar xz --strip-components 1 -C ~/.travis/oracle -f v2.0.2.tar.gz
+tar xz --strip-components 1 -C ~/.travis/oracle -f v2.0.3.tar.gz
 
 ~/.travis/oracle/download.sh
 ~/.travis/oracle/install.sh
