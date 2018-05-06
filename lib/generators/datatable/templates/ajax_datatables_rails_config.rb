30095db7afb0bad70fe879a4fa862fcab8e3e0f6
Nicolas Rodriguez <nicoladmin@free.fr>
2018-05-06 04:53:33 +0200

    Fix https://github.com/jbox-web/ajax-datatables-rails/issues/144

--- lib/generators/datatable/templates/ajax_datatables_rails_config.rb
+++ lib/generators/datatable/templates/ajax_datatables_rails_config.rb
@@ -4,6 +4,9 @@ AjaxDatatablesRails.configure do |config|
   # available options for db_adapter are: :pg, :mysql, :mysql2, :sqlite, :sqlite3
   # config.db_adapter = :pg
 
+  # Or you can use your rails environment adapter if you want a generic dev and production
+  # config.db_adapter = Rails.configuration.database_configuration[Rails.env]['adapter'].to_sym
+
   # available options for orm are: :active_record, :mongoid
   # config.orm = :active_record
 end
