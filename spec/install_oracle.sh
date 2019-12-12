633247a751654cec798b6b2265bc30b204dd9bf5
Nicolas Rodriguez <nicoladmin@free.fr>
2019-12-12 02:32:12 +0100

    Download Oracle package from a custom location

--- spec/install_oracle.sh
+++ spec/install_oracle.sh
@@ -4,7 +4,12 @@ wget 'https://github.com/cbandy/travis-oracle/archive/v2.0.3.tar.gz'
 mkdir -p ~/.travis/oracle
 tar xz --strip-components 1 -C ~/.travis/oracle -f v2.0.3.tar.gz
 
-~/.travis/oracle/download.sh
+if [ -n $CUSTOM_ORACLE_FILE ]; then
+  wget -q $CUSTOM_ORACLE_FILE -O ~/.travis/oracle/oracle-xe-11.2.0-1.0.x86_64.rpm.zip
+else
+  ~/.travis/oracle/download.sh
+fi
+
 ~/.travis/oracle/install.sh
 
 "$ORACLE_HOME/bin/sqlplus" -L -S / AS SYSDBA <<SQL
