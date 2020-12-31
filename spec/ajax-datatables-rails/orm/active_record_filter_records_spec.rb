672cccf150489ce8cbbcfb29acaa8458cb3e6f9a
Nicolas Rodriguez <nicoladmin@free.fr>
2020-12-31 18:48:45 +0100

    Move test

--- spec/ajax-datatables-rails/orm/active_record_filter_records_spec.rb
+++ spec/ajax-datatables-rails/orm/active_record_filter_records_spec.rb
@@ -631,4 +631,20 @@ describe AjaxDatatablesRails::ORM::ActiveRecord do
       end
     end
   end
+
+  describe 'formatter option' do
+    let(:datatable) { DatatableWithFormater.new(sample_params) }
+
+    before(:each) do
+      create(:user, username: 'johndoe', email: 'johndoe@example.com', last_name: 'DOE')
+      create(:user, username: 'msmith', email: 'mary.smith@example.com', last_name: 'SMITH')
+      datatable.params[:columns]['3'][:search][:value] = 'doe'
+    end
+
+    it 'can transform search value before asking the database' do
+      expect(datatable.data.size).to eq 1
+      item = datatable.data.first
+      expect(item[:last_name]).to eq 'DOE'
+    end
+  end
 end
